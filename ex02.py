195f56bbac9e00cdb1f12f526c3ef5a8a1818771
Michel <106674127+MichelZero@users.noreply.github.com>
2022-08-29 17:08:45 -0300

    up

---
+++ ex02.py
@@ -0,0 +1,24 @@
+# autores
+# Leandro
+# Michel
+
+# data: 30/08/2022
+
+# 2 –  Faça programa que calcule o fatorial de um número.
+
+# https://pt.wikipedia.org/wiki/Fatorial
+# A função fatorial é normalmente definida por:
+# n! = n(n-1)*n(n-2)*...*3*2*1
+
+# calculando dee forma interativa com o FOR
+valor = int(input("informe um número: "))
+
+# calculando o fatorial do valor informado
+fatorial = 1
+
+for i in range(1, valor + 1):
+  fatorial = fatorial * i
+  
+# saída de dados
+print(f"o fatorial do valor {valor} = {fatorial}")
+print("fim do programa!")
