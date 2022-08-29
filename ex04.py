70ccb81ed9719eb0dc5da7e48bedab7aca7da742
Michel <106674127+MichelZero@users.noreply.github.com>
2022-08-29 18:18:01 -0300

    up

--- ex04.py
+++ ex04.py
@@ -8,6 +8,7 @@
 # usuário e informe quantas vogais estão presentes 
 # neste texto.
 
+###   1   ###
 # entrada
 texto = input("Digite uma mensagem: ")
 quantidade = 0
@@ -15,6 +16,19 @@ quantidade = 0
 # processamento
 for i in texto:
   if (i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u'):
+    quantidade +=1
+    
+# saída
+print(f"a quantidade de vogais do texto é {quantidade}")
+print("fim do programa")
+
+
+###   2   ###
+quantidade = 0
+vogais = {'a', 'e', 'i', 'o', 'u'} # dicionário
+# processamento
+for i in texto:
+  if i in vogais:
     quantidade +=1
     
 # saída
