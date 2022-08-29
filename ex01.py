97a0db83b88459e7adeca6304fab8ba398fb287f
Michel <106674127+MichelZero@users.noreply.github.com>
2022-08-29 16:33:12 -0300

    first commit

---
+++ ex01.py
@@ -0,0 +1,21 @@
+# autores
+# Leandro
+# Michel
+
+# data: 30/08/2022
+
+# 1 – Imprima de 1 a 9.
+
+inicio = 1
+parada = 9
+
+######   1   #######
+print("###  1  ###")
+for i in range(parada + 1):
+  print(f"i[{i}] -> {i}")
+
+
+######   2   #######
+print("###  2  ###")
+for i in range(inicio, parada + 1):
+  print(f"i[{i}] --> {i}")
