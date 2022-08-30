e68ad26350cafe9daf3a348f15353176160c6a47
Michel <106674127+MichelZero@users.noreply.github.com>
2022-08-30 14:21:06 -0300

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -6,6 +6,10 @@
 
 # 1 – Imprima de 1 a 9.
 
+# range(parada)
+# range(inicio, parada)
+# range(inicio, parada, passo)
+
 inicio = 1
 parada = 9
 
@@ -19,3 +23,11 @@ for i in range(parada + 1):
 print("###  2  ###")
 for i in range(inicio, parada + 1):
   print(f"i[{i}] --> {i}")
+  
+###   3   ###
+inicio = 9
+parada = 0
+passo = -1
+
+for i in range(inicio, parada, passo):
+  print(f"valor[{i}] = {i}")
